47346d734e640b5c1fb9666581c5d9c6c93e052f
SarayutZ <wanoizloserBZ@gmail.com>
2023-09-20 11:58:43 +0700

    แก้ไข

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<h3>หวัดดี บอล</h3>
 </template>
 
 <style>
