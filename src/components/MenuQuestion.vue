21663d41faea443fc3d3d68a5cf70f4a2bdcdc78
Wattanapongphan <wattanapongphan@gmail.com>
2023-09-21 16:21:39 +0700

    create tabs

--- src/components/MenuQuestion.vue
+++ src/components/MenuQuestion.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-<p class="text-center">เริ่มได้เลย</p>
+<div class="container">
+    <div class="border p-3 shadow-sm d-flex justify-content-center">
+        <ul class="nav text-secondary">
+            <li class="p-2 g-col-6 text-dark " v-for="(item, index) in topic" :key="index">
+                <a>{{ item.name }}</a>
+            </li>
+        </ul>
     </div>
+</div>
 </template>
-
-<script>
-    export default {
-        
-    }
+  
+<script setup>
+import { ref } from "vue";
+const topic = ref([
+  { name: 'ทำไมการเงินถึงสำคัญ?'},
+  { name: 'ทำไมต้องออม?'},
+  { name: 'สิ่งที่ควรพิจารณาเมื่อเริ่มออมเงิน?'},
+  { name: 'ควรจะออมเงินทางไหน?'},
+  { name: 'การจัดการหนี้คืออะไร?'},]);
 </script>
 
 <style >
