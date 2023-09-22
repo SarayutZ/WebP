0f04cc2e2b2a8cfcdbc7e4438effaf6b0c0465c5
Wattanapongphan <wattanapongphan@gmail.com>
2023-09-22 14:56:54 +0700

    Create MenuQuestion

--- src/components/MenuQuestion.vue
+++ src/components/MenuQuestion.vue
@@ -1,15 +1,13 @@
 <template>
-<div class="container">
-    <div class="border p-3 shadow-sm d-flex justify-content-center">
-        <ul class="nav text-secondary">
-            <li class="p-2 g-col-6 text-dark " v-for="(item, index) in topic" :key="index">
-                <a>{{ item.name }}</a>
-            </li>
-        </ul>
-    </div>
+<div class="border p-3 shadow-sm d-flex justify-content-center ">
+    <ul class="nav text-secondary ">
+        <li class="p-2 g-col-6 text-dark " v-for="(item, index) in topic" :key="index">
+            <a>{{ item.name }}</a>
+        </li>
+    </ul>
 </div>
 </template>
-  
+
 <script setup>
 import { ref } from "vue";
 const topic = ref([
